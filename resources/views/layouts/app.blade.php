db779523410e715db2685470884bc7169e468726
foisal ahmed <foisal.cse@gmail.com>
2021-06-20 11:10:31 +0600

    Frontend Integrated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,268 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>NextPage - Blog, Magazine Html Template</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <link rel=icon href="{{ asset('public/frontend/img/favicon.png') }}') }}" sizes="20x20" type="image/png">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/frontend/css/vendor.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/frontend/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/frontend/css/responsive.css') }}">
+</head>
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+<body>
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+    <div class="preloader" id="preloader">
+        <div class="preloader-inner">
+            <div class="spinner">
+                <div class="dot1"></div>
+                <div class="dot2"></div>
+            </div>
         </div>
-    </body>
+    </div>
+
+    <div class="td-search-popup" id="td-search-popup">
+        <form action="https://solverwp.com/demo/html/nextpage/index.html" class="search-form">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Search.....">
+            </div>
+            <button type="submit" class="submit-btn"><i class="fa fa-search"></i></button>
+        </form>
+    </div>
+
+    <div class="body-overlay" id="body-overlay"></div>
+
+    <div class="navbar-area">
+
+        <div class="topbar-area">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6 col-md-7 align-self-center">
+                        <div class="topbar-menu text-md-left text-center">
+                            <ul class="align-self-center">
+                                <li><a href="#">Author</a></li>
+                                <li><a href="#">Advertisment</a></li>
+                                <li><a href="#">Member</a></li>
+                                <li><a href="#">Sitemap</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-5 mt-2 mt-md-0 text-md-right text-center">
+                        <div class="topbar-social">
+                            <div class="topbar-date d-none d-lg-inline-block"><i class="fa fa-calendar"></i> Saturday,
+                                October 7</div>
+                            <ul class="social-area social-area-2">
+                                <li><a class="facebook-icon" href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a class="twitter-icon" href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a class="youtube-icon" href="#"><i class="fa fa-youtube-play"></i></a></li>
+                                <li><a class="instagram-icon" href="#"><i class="fa fa-instagram"></i></a></li>
+                                <li><a class="google-icon" href="#"><i class="fa fa-google-plus"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="adbar-area bg-black d-none d-lg-block">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-5 align-self-center">
+                        <div class="logo text-md-left text-center">
+                            <a class="main-logo" href="index-2.html"><img src="{{ asset('public/frontend/img/logo.png') }}" alt="img"></a>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-7 text-md-right text-center">
+                        <a href="#" class="adbar-right">
+                            <img src="{{ asset('public/frontend/img/add/1.png') }}" alt="img">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <nav class="navbar navbar-expand-lg">
+            <div class="container nav-container">
+                <div class="responsive-mobile-menu">
+                    <div class="logo d-lg-none d-block">
+                        <a class="main-logo" href="index-2.html"><img src="{{ asset('public/frontend/img/logo.png') }}" alt="img"></a>
+                    </div>
+                    <button class="menu toggle-btn d-block d-lg-none" data-target="#nextpage_main_menu"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-left"></span>
+                        <span class="icon-right"></span>
+                    </button>
+                </div>
+                <div class="nav-right-part nav-right-part-mobile">
+                    <a class="search header-search" href="#"><i class="fa fa-search"></i></a>
+                </div>
+                <div class="collapse navbar-collapse" id="nextpage_main_menu">
+                    <ul class="navbar-nav menu-open">
+                        <li class="current-menu-item">
+                            <a href="#">Home</a>
+                        </li>
+                        <li class="current-menu-item">
+                            <a href="#">Category</a>
+                        </li>
+                        <li class="current-menu-item">
+                            <a href="#">Pages</a>
+                        </li>
+                        <li class="current-menu-item">
+                            <a href="#">Blog</a>
+                        </li>
+                        <li class="current-menu-item">
+                            <a href="#">Home</a>
+                        </li>
+                        <li class="current-menu-item">
+                            <a href="#">Category</a>
+                        </li>
+                        <li class="current-menu-item">
+                            <a href="#">Pages</a>
+                        </li>
+                        <li class="current-menu-item">
+                            <a href="#">Blog</a>
+                        </li>
+                        <li class="current-menu-item">
+                            <a href="#">Blog</a>
+                        </li>
+                        
+                    </ul>
+                </div>
+                <div class="nav-right-part nav-right-part-desktop">
+                    <div class="menu-search-inner">
+                        <input type="text" placeholder="Search For">
+                        <button type="submit" class="submit-btn"><i class="fa fa-search"></i></button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+	@section('frontend-content')
+		
+	@show
+
+    <div class="footer-area bg-black pd-top-95">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-sm-6">
+                    <div class="widget">
+                        <h5 class="widget-title">ABOUT US</h5>
+                        <div class="widget_about">
+                            <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                                aliqua. Quis ipsum ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel
+                                facilisis.</p>
+                            <ul class="social-area social-area-2 mt-4">
+                                <li><a class="facebook-icon" href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a class="twitter-icon" href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a class="youtube-icon" href="#"><i class="fa fa-youtube-play"></i></a></li>
+                                <li><a class="instagram-icon" href="#"><i class="fa fa-instagram"></i></a></li>
+                                <li><a class="google-icon" href="#"><i class="fa fa-google-plus"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="widget widget_tag_cloud">
+                        <h5 class="widget-title">TAGS</h5>
+                        <div class="tagcloud">
+                            <a href="#">Consectetur</a>
+                            <a href="#">Video</a>
+                            <a href="#">App</a>
+                            <a href="#">Food</a>
+                            <a href="#">Game</a>
+                            <a href="#">Internet</a>
+                            <a href="#">Phones</a>
+                            <a href="#">Development</a>
+                            <a href="#">Video</a>
+                            <a href="#">Game</a>
+                            <a href="#">Gadgets</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="widget">
+                        <h5 class="widget-title">CONTACTS</h5>
+                        <ul class="contact_info_list">
+                            <li><i class="fa fa-map-marker"></i> 829 Cabell Avenue Arlington, VA 22202</li>
+                            <li><i class="fa fa-phone"></i> +088 012121240</li>
+                            <li><i class="fa fa-envelope-o"></i> <a
+                                    href="https://solverwp.com/cdn-cgi/l/email-protection" class="__cf_email__"
+                                    data-cfemail="1f567179705f687a7d6c766b7a317c7072">[email&#160;protected]</a> <br> <a
+                                    href="https://solverwp.com/cdn-cgi/l/email-protection" class="__cf_email__"
+                                    data-cfemail="efbc9a9f9f809d9baf828e8683c18c8082">[email&#160;protected]</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="widget widget_recent_post">
+                        <h5 class="widget-title">POPULAR NEWS</h5>
+                        <div class="single-post-list-wrap style-white">
+                            <div class="media">
+                                <div class="media-left">
+                                    <img src="{{ asset('public/frontend/img/post/list/1.png') }}" alt="img">
+                                </div>
+                                <div class="media-body">
+                                    <div class="details">
+                                        <div class="post-meta-single">
+                                            <ul>
+                                                <li><i class="fa fa-clock-o"></i>08.22.2020</li>
+                                            </ul>
+                                        </div>
+                                        <h6 class="title"><a href="blog-details.html">Himachal Pradesh rules in order to
+                                                allow tourists </a></h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="single-post-list-wrap style-white">
+                            <div class="media">
+                                <div class="media-left">
+                                    <img src="{{ asset('public/frontend/img/post/list/2.png') }}" alt="img">
+                                </div>
+                                <div class="media-body">
+                                    <div class="details">
+                                        <div class="post-meta-single">
+                                            <ul>
+                                                <li><i class="fa fa-clock-o"></i>08.22.2020</li>
+                                            </ul>
+                                        </div>
+                                        <h6 class="title"><a href="blog-details.html">Himachal Pradesh rules in order to
+                                                allow tourists </a></h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom text-center">
+                <ul class="widget_nav_menu">
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Terms & Conditions</a></li>
+                    <li><a href="#">rivacy Policy</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <p>Copyright ©2021 Nextpage</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="back-to-top">
+        <span class="back-top"><i class="fa fa-angle-up"></i></span>
+    </div>
+
+    <script src="{{ asset('public/frontend/js/vendor.js') }}"></script>
+
+    <script src="{{ asset('public/frontend/js/main.js') }}"></script>
+</body>
+
 </html>
